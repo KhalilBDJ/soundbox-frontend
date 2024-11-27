e0a52d77cded2b9c225896768383c317216b2c6a
KhalilBDJ <khalilobedjaoui@gmail.com>
2024-11-27 15:55:49 +0100

    Adding components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {BoxComponent} from './box/box.component';
+import {MainContainerComponent} from './layouts/main-container/main-container.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BoxComponent],
+  imports: [RouterOutlet, BoxComponent, MainContainerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/box/box.component.ts
+++ src/app/box/box.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import {ModifyButtonComponent} from '../buttons/modify-button/modify-button.component';
+import {PlayButtonComponent} from '../buttons/play-button/play-button.component';
+import {
+  HorizontalButtonsLayoutComponent
+} from '../layouts/horizontal-buttons-layout/horizontal-buttons-layout.component';
 
 @Component({
   selector: 'app-box',
   standalone: true,
-  imports: [],
+  imports: [
+    ModifyButtonComponent,
+    PlayButtonComponent,
+    HorizontalButtonsLayoutComponent
+  ],
   templateUrl: './box.component.html',
   styleUrl: './box.component.css'
 })
