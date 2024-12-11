1a1427b42dd849b027d0f9625aaab0512b7a3e6a
KhalilBDJ <khalilobedjaoui@gmail.com>
2024-12-11 17:58:21 +0100

    Need to fix wavesurfer

--- src/app/audio-spectrum/audio-spectrum.component.ts
+++ src/app/audio-spectrum/audio-spectrum.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnChanges, Output, SimpleChanges} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import WaveSurfer from 'wavesurfer.js';
 import RegionsPlugin from 'wavesurfer.js/plugins/regions';
 
@@ -9,81 +9,61 @@ import RegionsPlugin from 'wavesurfer.js/plugins/regions';
   templateUrl: './audio-spectrum.component.html',
   styleUrls: ['./audio-spectrum.component.css']
 })
-export class AudioSpectrumComponent implements OnChanges {
-  @Input() audioBlob: Blob | null = null;
-  @Output() regionChange = new EventEmitter<{start: number; end: number}>();
-
+export class AudioSpectrumComponent {
   private waveSurfer: any;
   private regionsPlugin: any;
-  private activeRegion: any;
+  @Input() audioBlob!: Blob | null;
+  @Output() regionChange = new EventEmitter<{ start: number; end: number }>();
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['audioBlob'] && this.audioBlob) {
-      this.initWaveSurfer();
-    }
-  }
-
-  private initWaveSurfer() {
-    if (this.waveSurfer) {
-      this.waveSurfer.destroy();
-    }
-
+  ngOnInit() {
+    // Initialisation de WaveSurfer avec le plugin Regions
     this.regionsPlugin = RegionsPlugin.create();
-
-    const blobUrl = URL.createObjectURL(this.audioBlob!);
 
     this.waveSurfer = WaveSurfer.create({
       container: '#waveform',
       waveColor: 'rgb(200, 0, 200)',
       progressColor: 'rgb(100, 0, 100)',
-      backend: 'WebAudio',
+      url: '/assets/audio/wet_fart.mp3',
       plugins: [this.regionsPlugin],
     });
 
-    this.waveSurfer.load(blobUrl);
-
     this.waveSurfer.on('ready', () => {
+      // Obtenir la durée totale de l'audio
       const audioDuration = this.waveSurfer.getDuration();
-      this.activeRegion = this.regionsPlugin.addRegion({
+
+      // Ajouter une région couvrant toute la durée de l'audio
+      this.regionsPlugin.addRegion({
         start: 0,
-        end: audioDuration,
+        end: 1.5,
         color: 'rgba(200, 0, 200, 0.5)',
         drag: true,
         resize: true,
       });
 
-      this.waveSurfer.on('region-update-end', (region: any) => {
-        this.activeRegion = region;
-        this.emitRegionChange();
+      this.regionsPlugin.on('region-out',(e:any)=>{
+        console.log("region exited");
+        this.waveSurfer.pause();
+        this.waveSurfer.seekTo(e.start /this.waveSurfer.getDuration());
       });
+    });
 
-      // Gérer le stop à la fin de la région
-      this.waveSurfer.on('audioprocess', () => {
-        if (this.activeRegion) {
-          const currentTime = this.waveSurfer.getCurrentTime();
-          if (currentTime > this.activeRegion.end) {
-            this.waveSurfer.pause();
-            this.waveSurfer.seekTo(this.activeRegion.start / this.waveSurfer.getDuration());
-          }
-        }
-      });
-
-      // Émettre la région initiale
-      this.emitRegionChange();
+    // Bouton pour jouer la région sélectionnée
+    const playRegionButton = document.getElementById('play-region') as HTMLButtonElement;
+    playRegionButton.addEventListener('click', () => {
+      this.playRegion();
     });
   }
 
-  playRegion() {
-    if (this.activeRegion) {
-      this.waveSurfer.play(this.activeRegion.start, this.activeRegion.end);
+  private playRegion() {
+    const regions = this.regionsPlugin.getRegions();
+    const regionKeys = Object.keys(regions);
+
+    if (regionKeys.length > 0) {
+      const region = regions[regionKeys[0]];// Utiliser la première région disponible
+      console.log(region.end + " " + region.start);
+      region.play();
     } else {
       console.warn('No regions available to play.');
-    }
-  }
-
-  private emitRegionChange() {
-    if (this.activeRegion) {
-      this.regionChange.emit({ start: this.activeRegion.start, end: this.activeRegion.end });
     }
   }
 }
